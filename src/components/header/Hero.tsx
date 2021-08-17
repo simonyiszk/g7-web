c1f06a9e5481cafc8014d71aae72f9ee684b1c80
Andris Borbas <andris.borbas@gmail.com>
2021-08-18 01:35:46 +0200

    Socials open on new tab

--- src/components/header/Hero.tsx
+++ src/components/header/Hero.tsx
@@ -9,10 +9,18 @@ export function Hero() {
 			<h1 className="my-4 text-5xl">Gólyahét 2021</h1>
 			<h3 className="text-2xl">08.30. - 09.03.</h3>
 			<div className="flex justify-evenly mx-auto mt-12 w-full lg:w-1/3">
-				<a href="https://www.instagram.com/bme_vik_golyahet/">
+				<a
+					href="https://www.instagram.com/bme_vik_golyahet/"
+					target="_blank"
+					rel="noreferrer"
+				>
 					<FaInstagram className="w-16 h-auto hover:text-orange-600" />
 				</a>
-				<a href="https://www.facebook.com/bmevikgolyahet">
+				<a
+					href="https://www.facebook.com/bmevikgolyahet"
+					target="_blank"
+					rel="noreferrer"
+				>
 					<FaFacebookSquare className="w-16 h-auto hover:text-orange-600" />
 				</a>
 			</div>
