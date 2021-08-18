aece9aec42b8581f7cb7e3c73aa33e143a0d9971
Andris Borbas <andris.borbas@gmail.com>
2021-08-18 23:16:38 +0200

    Add meta

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,40 +33,8 @@ export function Layout({ children, title, ...restProps }: LayoutProps) {
 
 				<link
 					rel="apple-touch-icon"
-					sizes="60x60"
-					href="/apple-touch-icon-60x60.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="76x76"
-					href="/apple-touch-icon-76x76.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="120x120"
-					href="/apple-touch-icon-120x120.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="152x152"
-					href="/apple-touch-icon-152x152.png"
-				/>
-				<link
-					rel="apple-touch-icon"
 					sizes="180x180"
-					href="/apple-touch-icon-180x180.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="512x512"
-					href="/favicon-512x512.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="192x192"
-					href="/favicon-192x192.png"
+					href="/apple-touch-icon.png"
 				/>
 				<link
 					rel="icon"
@@ -77,22 +45,24 @@ export function Layout({ children, title, ...restProps }: LayoutProps) {
 				<link
 					rel="icon"
 					type="image/png"
+					sizes="192x192"
+					href="/android-chrome-192x192.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
 					sizes="16x16"
 					href="/favicon-16x16.png"
 				/>
 				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#161627" />
-				<meta name="apple-mobile-web-app-title" content="G7" />
-				<meta name="application-name" content="G7" />
-				<meta name="msapplication-TileColor" content="#161627" />
-				<meta name="theme-color" content="#161627" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d7752f" />
+				<meta name="msapplication-TileColor" content="#d7752f" />
+				<meta name="theme-color" content="#d7752f" />
 
 				<meta name="mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-status-bar-style" content="default" />
 				<meta name="format-detection" content="telephone=yes" />
-				<link rel="manifest" href="/manifest.json" />
-				<link rel="shortcut icon" href="/favicon-100x100.png" />
 			</Head>
 
 			<Navbar />
