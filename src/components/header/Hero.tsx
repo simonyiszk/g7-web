99e4b44040def1a73499e0ca16f2d9d21fea664a
Andris Borbas <andris.borbas@gmail.com>
2021-08-19 19:14:33 +0200

    Make hero larger

--- src/components/header/Hero.tsx
+++ src/components/header/Hero.tsx
@@ -4,9 +4,9 @@ import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
 export function Hero() {
 	return (
 		<section className="flex flex-col justify-center items-center my-4 mb-8 w-full min-h-[calc(100vh-72px)] text-center">
-			<h2 className="text-3xl">BME-VIK</h2>
-			<h1 className="my-4 text-5xl">Gólyahét 2021</h1>
-			<h3 className="text-2xl">08.30. - 09.03.</h3>
+			<h2 className="text-3xl lg:text-4xl uppercase">BME-VIK</h2>
+			<h1 className="my-4 text-5xl lg:text-6xl uppercase">Gólyahét 2021</h1>
+			<h3 className="text-2xl lg:text-3xl">08.30. - 09.03.</h3>
 			<div className="flex justify-evenly mx-auto mt-12 w-full lg:w-1/3">
 				<a
 					href="https://www.instagram.com/bme_vik_golyahet/"
