353951f0cd5f00ee848a68c909fc3ea4ea77de5d
Andris Borbás <andris.borbas@gmail.com>
2021-08-22 16:47:00 +0200

    Add padding to contact page and add vodafone and hk logo to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,6 +7,8 @@ import schpng from "@/assets/images/sch.png";
 import schdesignpng from "@/assets/images/schdesign.png";
 import ssslpng from "@/assets/images/sssl.png";
 import vikpng from "@/assets/images/vik.png";
+import vikhk from "@/assets/images/vikhk.png";
+import vodafone from "@/assets/images/vodafonewhite.png";
 import { Fountain } from "@/utils/fountain";
 
 import * as styles from "./Footer.module.scss";
@@ -15,13 +17,21 @@ export function Footer() {
 	const loveRef = React.useRef<HTMLSpanElement>(null);
 	const [clicks, setClicks] = React.useState(0);
 	React.useEffect(() => {
-		if (clicks > 1 && loveRef.current) {
-			setClicks(0);
+		if (clicks !== 0 && clicks % 2 === 0 && loveRef.current) {
 			const f = new Fountain(
 				loveRef.current.getBoundingClientRect().left,
 				loveRef.current.getBoundingClientRect().top,
 				loveRef.current,
 				777,
+			);
+		}
+		if (clicks === 13) {
+			window.open("https://i.imgur.com/OucAB1l.jpg", "_blank");
+		}
+		if (clicks === 25) {
+			window.open(
+				"https://i.pinimg.com/originals/8e/6c/83/8e6c83e94bd52d56c302907806c8ecde.png",
+				"_blank",
 			);
 		}
 		return () => {};
@@ -34,7 +44,22 @@ export function Footer() {
 			)}
 		>
 			<div className="container grid grid-cols-1 justify-center content-center mx-auto text-center">
-				<div className="flex flex-wrap flex-auto justify-center content-center items-center mx-auto w-full">
+				<div className="flex flex-wrap flex-auto justify-center content-center items-center mx-auto w-full ">
+					<a href="https://vodafone.hu/" target="_blank" rel="noreferrer">
+						<div className="relative m-4 w-20 h-24 !filter-none">
+							<Image
+								src={vodafone}
+								alt="Vodafone logó"
+								layout="fill"
+								objectFit="contain"
+								objectPosition="center"
+								placeholder="blur"
+							/>
+						</div>
+					</a>
+
+					<div className="lg:block mx-8 mb-2 lg:mb-0 w-[80%] lg:w-0 lg:h-[80%] lg:border-r-2 border-b-2 lg:border-b-0 border-white" />
+
 					<a href="https://sssl.sch.bme.hu/" target="_blank" rel="noreferrer">
 						<div className="relative m-4 w-16 h-20">
 							<Image
@@ -47,11 +72,12 @@ export function Footer() {
 							/>
 						</div>
 					</a>
-					<a href="https://sch.bme.hu" target="_blank" rel="noreferrer">
-						<div className="relative m-4 w-24 h-16">
+
+					<a href="https://schdesign.hu" target="_blank" rel="noreferrer">
+						<div className="relative m-4 w-24 h-20">
 							<Image
-								src={schpng}
-								alt="Schönherz Zoltán Kollégium logó"
+								src={schdesignpng}
+								alt="schdesign logó"
 								layout="fill"
 								objectFit="contain"
 								objectPosition="center"
@@ -71,11 +97,23 @@ export function Footer() {
 							/>
 						</div>
 					</a>
-					<a href="https://schdesign.hu" target="_blank" rel="noreferrer">
-						<div className="relative m-4 w-24 h-20">
+					<a href="https://sch.bme.hu" target="_blank" rel="noreferrer">
+						<div className="relative m-4 w-24 h-16">
 							<Image
-								src={schdesignpng}
-								alt="schdesign logó"
+								src={schpng}
+								alt="Schönherz Zoltán Kollégium logó"
+								layout="fill"
+								objectFit="contain"
+								objectPosition="center"
+								placeholder="blur"
+							/>
+						</div>
+					</a>
+					<a href="https://vik.hk" target="_blank" rel="noreferrer">
+						<div className="relative m-4 w-16 h-16">
+							<Image
+								src={vikhk}
+								alt="VIK HK logó"
 								layout="fill"
 								objectFit="contain"
 								objectPosition="center"
@@ -101,7 +139,7 @@ export function Footer() {
 					{/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
 					<span
 						className="overflow-visible hover:text-red-500 hover:cursor-pointer select-none"
-						title="meow"
+						title="honk"
 						ref={loveRef}
 						onClick={() => setClicks(clicks + 1)}
 					>
--- src/pages/kapcsolat/index.tsx
+++ src/pages/kapcsolat/index.tsx
@@ -3,36 +3,39 @@ import { Layout } from "@/components/Layout";
 export default function ContactPage() {
 	return (
 		<Layout
-			className="container px-4 lg:px-32 xl:px-48 2xl:px-64 mx-auto"
+			className="container px-4 lg:px-32 xl:px-48 2xl:px-64 mx-auto mt-8"
 			title="Kapcsolat"
 		>
 			<section className="text-center">
-				<h1 className="mb-8 text-4xl font-bold">Kapcsolat</h1>
+				<h1 className="mb-12 text-4xl font-bold">Kapcsolat</h1>
 				<h2 className="mb-2 text-3xl">Az esemény főrendezői:</h2>
 				<h3 className="text-2xl">Harmat Dominik</h3>
-				<h3 className="mb-4 text-2xl">Vass Viktor</h3>
-				<h4 className="mb-4 text-xl">
+				<h3 className="mb-10 text-2xl">Vass Viktor</h3>
+				<h4 className="mb-8 text-xl">
 					Ha bármilyen kérdésetek felmerül a programtervvel vagy az eseménnyel
 					kapcsolatban, akkor a{" "}
 					<a
 						href="mailto:golyahet2021@sch.bme.hu"
-						className="hover:text-orange-400 underline"
+						className="hover:text-orange-600 dark:hover:text-orange-400 underline"
+						rel="noreferrer"
 					>
 						golyahet2021@sch.bme.hu
 					</a>{" "}
 					e-mail címen tudtok tájékozódni.
 				</h4>
-				<h5 className="mb-4 text-lg">
+				<h5 className="mb-8 text-lg">
 					A 2021-es villanykaros gólyák facebook csoportját ezen a linken
 					keresztül éritek el:{" "}
 					<a
 						href="https://www.facebook.com/groups/bmevik2021"
-						className="hover:text-orange-400 underline"
+						className="hover:text-orange-600 dark:hover:text-orange-400 underline break-words"
+						target="_blank"
+						rel="noreferrer"
 					>
 						https://www.facebook.com/groups/bmevik2021
 					</a>
 				</h5>
-				<h5 className="mb-4 text-lg">
+				<h5 className="mb-8 text-lg">
 					A Gólyahét hivatalos facebook eseménye HAMAROSAN elérhető lesz.
 				</h5>
 			</section>
